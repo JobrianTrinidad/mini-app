eeaf0ee90d2ba8468e716e77bb390875c258aa50
rosauromatib <rosauromatibagjr@gmail.com>
2023-10-19 06:50:08 +0800

    update pom.xml

--- mini-app-core/src/main/java/com/aat/application/form/CommonForm.java
+++ mini-app-core/src/main/java/com/aat/application/form/CommonForm.java
@@ -4,8 +4,10 @@ import com.aat.application.core.StandardForm;
 import com.aat.application.core.ZJTEntity;
 import com.aat.application.core.ZJTService;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class CommonForm<T extends ZJTEntity> extends StandardForm<T, ZJTService<T>> {
-	public CommonForm(Class<T> entityClass, ZJTService<T> service) {
+	public CommonForm(Class<T> entityClass, ZJTService<T> service) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
 		super(entityClass, service);
 		addClassName("demo-app-form");
 	}
