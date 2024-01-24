19638fe54b64e738418a1cee6b56070747f57f1f
Jobrian Trinidad <jtrinidad@adaxa.com>
2024-01-24 15:37:05 +1100

    Start to document on how to define columns

--- mini-app-core/src/main/java/com/aat/application/core/data/entity/ZJTEntity.java
+++ mini-app-core/src/main/java/com/aat/application/core/data/entity/ZJTEntity.java
@@ -2,4 +2,24 @@ package com.aat.application.core.data.entity;
 
 public interface ZJTEntity {
     int getId();
+
+
+    /**
+     * id = tablename + _id  e.g zjt_vehicle = zjt_vehicle_id
+     *
+     * date use LocalDateTime
+     *
+     * foreign key use classname of the parent table
+     *      e.g.
+     *      @ManyToOne
+     *      @JoinColumn(name="zjt_user_id")
+     *      @DisplayName(value="Logged By")
+     *      private ZJTUser user
+     *
+     * If this Entity will be used as dropdown - add this annotation
+     *     @Column
+     *     @ContentDisplayedInSelect(value = "name")
+     *     @DisplayName(value = "Name")
+     *     private String name = "";
+     */
 }
