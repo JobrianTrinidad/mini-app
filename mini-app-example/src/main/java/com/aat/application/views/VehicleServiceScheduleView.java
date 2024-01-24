872d3ca4e0b3a20d89f50cbc73e1555fffce138b
Jobrian Trinidad <jtrinidad@adaxa.com>
2024-01-24 15:40:05 +1100

    Stub to add custom buttons for processing

--- mini-app-example/src/main/java/com/aat/application/views/VehicleServiceScheduleView.java
+++ mini-app-example/src/main/java/com/aat/application/views/VehicleServiceScheduleView.java
@@ -4,6 +4,10 @@ import com.aat.application.core.form.TimeLineViewParameter;
 import com.aat.application.data.entity.ZJTVehicleServiceSchedule;
 import com.aat.application.data.repository.BaseEntityRepository;
 import com.aat.application.data.service.TableInfoService;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "service-schedule")
@@ -14,5 +18,34 @@ public class VehicleServiceScheduleView extends StandardFormView<ZJTVehicleServi
         TimeLineViewParameter timeLineViewParameter =  new TimeLineViewParameter("timelineItemTitle", "vehicle", "planDate", null, null, "ZJTVehicleServiceSchedule");
         timeLineViewParameter.setWhereDefinition("vehicle.zjt_vehicle_id");
         super.setTimeLineViewParameter(timeLineViewParameter);
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event)
+    {
+        super.beforeEnter(event);
+
+        Button button = new Button();
+        button.setIcon(new Icon(VaadinIcon.START_COG));
+        button.setTooltipText("Create workshop job.");
+        button.addClickListener(e -> createWorkshopJob());
+        this.addCustomButton(button);
+
+        button = new Button();
+        button.setIcon(new Icon(VaadinIcon.STOP_COG));
+        button.setTooltipText("Complete current workshop job.");
+        button.addClickListener(e -> completeWorkshopJob());
+        this.addCustomButton(button);
+
+    }
+
+    private void createWorkshopJob()
+    {
+        this.setMessageStatus("Workshop job created.");
+    }
+
+    private  void completeWorkshopJob()
+    {
+        this.setMessageStatus("Workshop job complete.");
     }
 }
