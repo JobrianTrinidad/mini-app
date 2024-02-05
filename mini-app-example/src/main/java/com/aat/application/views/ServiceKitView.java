17e6403d2dcf6dbc76ff21152bdf117b7429014c
Jobrian Trinidad <jtrinidad@adaxa.com>
2024-02-05 14:02:52 +1100

    Fix code merging issue

--- mini-app-example/src/main/java/com/aat/application/views/ServiceKitView.java
+++ mini-app-example/src/main/java/com/aat/application/views/ServiceKitView.java
@@ -1,10 +1,6 @@
 package com.aat.application.views;
 
 import com.aat.application.core.data.entity.ZJTEntity;
-<<<<<<< HEAD
-import com.aat.application.data.repository.BaseEntityRepository;
-import com.aat.application.data.service.TableInfoService;
-=======
 import com.aat.application.core.form.CommonForm;
 import com.aat.application.core.form.GridViewParameter;
 import com.aat.application.core.form.TimeLineViewParameter;
@@ -17,19 +13,11 @@ import com.vaadin.componentfactory.tuigrid.model.MenuItem;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.notification.Notification;
->>>>>>> mygit/main
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.OptionalParameter;
 import com.vaadin.flow.router.Route;
 
-<<<<<<< HEAD
-@Route(value = "service-kit/:subcategory?/:filter?", layout = MainLayout.class)
-public class ServiceKitView extends StandardFormView<ZJTEntity> implements HasUrlParameter<String> {
-
-    public ServiceKitView(BaseEntityRepository<ZJTEntity> repository, TableInfoService tableInfoService) {
-        super(repository, tableInfoService);
-=======
 import java.util.Optional;
 
 @Route(value = "service-kit/:subcategory?/:filter?", layout = MainLayout.class)
@@ -72,14 +60,11 @@ public class ServiceKitView extends StandardFormView implements HasUrlParameter<
                     this.setMessageStatus(count + " rows is deleted.");
             });
         }
->>>>>>> mygit/main
     }
 
 
     @Override
     public void setParameter(BeforeEvent event, @OptionalParameter String parameter) {
-<<<<<<< HEAD
-=======
         if (parameter != null) {
             if (event.getRouteParameters().get("subcategory").isPresent()
                     && event.getRouteParameters().get("subcategory").get().equals("service-type-kit")) {
@@ -109,7 +94,6 @@ public class ServiceKitView extends StandardFormView implements HasUrlParameter<
         timelineItem.addContextMenuClickListener(e -> UI.getCurrent().navigate("service-kit/service-type-kit/timeline/" + e.getRow().get(0).getRowKey()));
 
         this.setContextMenu(contextMenu);
->>>>>>> mygit/main
     }
 
 
